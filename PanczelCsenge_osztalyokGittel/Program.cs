6be8180c27dbe5cece709bf0e916bfa97bbf9709
Panczel24 <pcsenge8@gmail.com>
2025-02-07 16:45:55 +0100

    Próba, hogy itthon is megy e

--- PanczelCsenge_osztalyokGittel/Program.cs
+++ PanczelCsenge_osztalyokGittel/Program.cs
@@ -5,28 +5,13 @@
 		static void Main(string[] args)
 		{
 
-			//Konyves();
+            //Konyves();
+            //Filmes();
+            Jatekkarakterek();
 
-            Console.WriteLine();
-            Console.WriteLine("Film");
+            
 
-			Film film1 = new Film("Életrevalók", "Olivier Nakache", 108, "életrajzi dráma", true );
-			Film film2 = new Film("Az élettörténetem", "Én");
-			Console.WriteLine( film1.ToString() + "\n" + film2.ToString());
-			Console.WriteLine();
-			Console.WriteLine($"ELezdődött-e a film?:  \n  {film1.Cim} - {film1.Jatszas()}, {film2.Cim} - {film2.Jatszas()} ");
-			Console.WriteLine("\n" + film1.ToString() + "\n" + "új hossz:" + film1.HosszNovelese(20) +", új műfaj:" + film1.MufajModositas("komédia"));
-
-
-            Console.WriteLine();
-			Console.WriteLine("Videojatek karakter");
-			VideojatekKarakter karakter1 = new("Zebra", 20, 100, 50);
-			VideojatekKarakter karakter2 = new("Tigris", 70);
-			Console.WriteLine(karakter1.ToString() + "\n" + karakter2.ToString());
-            Console.WriteLine();
-			Console.WriteLine("\n" + karakter1.ToString() + "\n" + karakter1.Tamadas() + ",ennyi életereje van:" +karakter1.Gyogyulas(20));
-            Console.WriteLine(karakter1.Nev +"," + karakter1.Szintlepes());
-
+            
         }
 
 
@@ -43,8 +28,34 @@
 
 			Console.WriteLine("\n" + konyv1.ToString() + "\n" + "ennyi oldal maradt:" + konyv1.HanyOldalMaradt(20));
 
-
 		}
 
-	}
+		static void Filmes()
+		{
+            Console.WriteLine();
+            Console.WriteLine("Film");
+
+            Film film1 = new Film("Életrevalók", "Olivier Nakache", 108, "életrajzi dráma", true);
+            Film film2 = new Film("Az élettörténetem", "Én");
+            Console.WriteLine(film1.ToString() + "\n" + film2.ToString());
+            Console.WriteLine();
+            Console.WriteLine($"ELezdődött-e a film?:  \n  {film1.Cim} - {film1.Jatszas()}, {film2.Cim} - {film2.Jatszas()} ");
+            Console.WriteLine("\n" + film1.ToString() + "\n" + "új hossz:" + film1.HosszNovelese(20) + ", új műfaj:" + film1.MufajModositas("komédia"));
+
+        }
+
+		static void Jatekkarakterek()
+		{
+            Console.WriteLine();
+            Console.WriteLine("Videojatek karakter");
+            VideojatekKarakter karakter1 = new("Zebra", 20, 100, 50);
+            VideojatekKarakter karakter2 = new("Tigris", 70);
+            Console.WriteLine(karakter1.ToString() + "\n" + karakter2.ToString());
+            Console.WriteLine();
+            Console.WriteLine("\n" + karakter1.ToString() + "\n" + karakter1.Tamadas() + ",ennyi életereje van:" + karakter1.Gyogyulas(20));
+            Console.WriteLine(karakter1.Nev + "," + karakter1.Szintlepes());
+
+        }
+
+    }
 }
