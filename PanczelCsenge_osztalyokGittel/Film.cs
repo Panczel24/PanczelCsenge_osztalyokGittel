dacef48e32fedbdd349b1926d915d216c24aec89
Panczel24 <pcsenge8@gmail.com>
2025-02-07 12:35:29 +0100

    Film osztályt létrehoztam 2 konstrktorral, 3 metódussal

---
+++ PanczelCsenge_osztalyokGittel/Film.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanczelCsenge_osztalyokGittel
+{
+	internal class Film
+	{
+		//ezt kell kijelölni
+		private string cim;
+		private string rendezo;
+		private int hosszPercekben;
+		private string mufaj;
+		private bool megjelent;
+				
+
+		//konstruktor
+		public Film(string cim, string rendezo, int hosszPercekben, string mufaj, bool megjelent)
+		{
+			this.cim = cim;  
+			this.rendezo = rendezo;
+			this.hosszPercekben = hosszPercekben;
+			this.mufaj = mufaj;
+			this.megjelent = megjelent;
+		}
+
+		public Film(string cim, string rendezo)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			hosszPercekben = 120;
+			mufaj = "Ismeretlen";
+			megjelent = false;
+		}
+
+		//property
+		public string Cim { get => cim; set => cim = value; }
+		public string Rendezo { get => rendezo; set => rendezo = value; }
+		public int HosszPercekben { get => hosszPercekben; set => hosszPercekben = value; }
+		public string Mufaj { get => mufaj; set => mufaj = value; }
+		public bool Megjelent { get => megjelent; set => megjelent = value; }
+
+		//metodus
+		public string Jatszas()
+		{
+			if (megjelent == true)
+			{
+                return ("A film elkezdődött");
+			}
+			else
+			{
+				return ("A film nem kezdődött el");
+			}
+		}
+
+		public string MufajModositas(string ujMufaj)
+		{
+			return mufaj = ujMufaj;
+		}
+
+		public int HosszNovelese(int percek)
+		{
+			return hosszPercekben += percek;
+		}
+
+		public override string? ToString()
+		{
+			 return $"{cim} - {rendezo} / hossz: {hosszPercekben} / műfaj: {mufaj} - megjelent: {megjelent}";;
+		}
+
+
+		//tostring
+
+
+	}
+}
--- PanczelCsenge_osztalyokGittel/Program.cs
+++ PanczelCsenge_osztalyokGittel/Program.cs
@@ -15,6 +15,19 @@
 			Console.WriteLine("\n" + konyv1.ToString() + "\n" + "ennyi oldal maradt:" + konyv1.HanyOldalMaradt(20));
 
 
+
+
+            Console.WriteLine();
+            Console.WriteLine("Film");
+
+			Film film1 = new Film("Életrevalók", "Olivier Nakache", 108, "életrajzi dráma", true );
+			Film film2 = new Film("Az élettörténetem", "Én");
+			Console.WriteLine( film1.ToString() + "\n" + film2.ToString());
+			Console.WriteLine();
+			Console.WriteLine($"ELezdődött-e a film?:  \n  {film1.Cim} - {film1.Jatszas()}, {film2.Cim} - {film2.Jatszas()} ");
+			Console.WriteLine("\n" + film1.ToString() + "\n" + "új hossz:" + film1.HosszNovelese(20) +", új műfaj:" + film1.MufajModositas("komédia"));
+
+
 		}
 	}
 }
