7500c062fd9adc8e180180de3d26e300640eb981
Panczel24 <pcsenge8@gmail.com>
2025-02-07 13:00:44 +0100

    Videojatekkarakter osztály kész, 2 konstruktorral 3 metódussal

--- PanczelCsenge_osztalyokGittel/Program.cs
+++ PanczelCsenge_osztalyokGittel/Program.cs
@@ -4,18 +4,8 @@
 	{
 		static void Main(string[] args)
 		{
-			Konyv konyv1 = new Konyv("A kis herceg", "Antoine de Saint-Exupéry", 1943, 96, 2500 );
-			Konyv konyv2 = new ("Ez még készül", "Én" ); //ki lehet hagyni a konyv-classt, elég a new
 
-            Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
-
-			konyv1.Kedvezmeny(20);
-			konyv1.Arnoveles(100);
-
-			Console.WriteLine("\n" + konyv1.ToString() + "\n" + "ennyi oldal maradt:" + konyv1.HanyOldalMaradt(20));
-
-
-
+			//Konyves();
 
             Console.WriteLine();
             Console.WriteLine("Film");
@@ -28,6 +18,33 @@
 			Console.WriteLine("\n" + film1.ToString() + "\n" + "új hossz:" + film1.HosszNovelese(20) +", új műfaj:" + film1.MufajModositas("komédia"));
 
 
+            Console.WriteLine();
+			Console.WriteLine("Videojatek karakter");
+			VideojatekKarakter karakter1 = new("Zebra", 20, 100, 50);
+			VideojatekKarakter karakter2 = new("Tigris", 70);
+			Console.WriteLine(karakter1.ToString() + "\n" + karakter2.ToString());
+            Console.WriteLine();
+			Console.WriteLine("\n" + karakter1.ToString() + "\n" + karakter1.Tamadas() + ",ennyi életereje van:" +karakter1.Gyogyulas(20));
+            Console.WriteLine(karakter1.Nev +"," + karakter1.Szintlepes());
+
+        }
+
+
+
+		static void Konyves()
+		{
+			Konyv konyv1 = new Konyv("A kis herceg", "Antoine de Saint-Exupéry", 1943, 96, 2500);
+			Konyv konyv2 = new("Ez még készül", "Én"); //ki lehet hagyni a konyv-classt, elég a new
+
+			Console.WriteLine(konyv1.ToString() + "\n" + konyv2.ToString());
+
+			konyv1.Kedvezmeny(20);
+			konyv1.Arnoveles(100);
+
+			Console.WriteLine("\n" + konyv1.ToString() + "\n" + "ennyi oldal maradt:" + konyv1.HanyOldalMaradt(20));
+
+
 		}
+
 	}
 }
---
+++ PanczelCsenge_osztalyokGittel/VideojatekKarakter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanczelCsenge_osztalyokGittel
+{
+	internal class VideojatekKarakter
+	{//ezt kell kijelölni
+		private string nev;
+		private int szint;
+		private int eletero;
+		private int ero;
+
+
+		//konstruktor
+		public VideojatekKarakter(string nev, int szint, int eletero, int ero)
+		{
+			this.nev = nev;
+			this.szint = szint;
+			this.eletero = eletero;
+			this.ero = ero;
+		}
+		public VideojatekKarakter(string nev, int ero)
+		{
+			this.nev = nev;
+			szint = 1;
+			eletero = 100;
+			this.ero = ero;
+		}
+
+		//property
+		public string Nev { get => nev; set => nev = value; }
+		public int Szint { get => szint; set => szint = value; }
+		public int Eletero { get => eletero; set => eletero = value; }
+		public int Ero { get => ero; set => ero = value; }
+
+
+		//metódus
+		public string Tamadas()
+		{
+			return ($"a karakter támad, ennyi erővel: {ero}");
+		}
+
+
+		public int Gyogyulas(int mennyiseg)
+		{
+			return eletero += mennyiseg;
+		}
+
+		public string Szintlepes()
+		{
+           return $"Az életerőd ennyi lett: {eletero+=30}, szintet léptél eggyel, új szint: {szint+=1}"; 
+		}
+
+		//tostring
+		public override string? ToString()
+		{
+			return $"{nev} , szint: {szint} / eleterő: {eletero} / ero: {ero} "; ;
+		}
+	}
+}
